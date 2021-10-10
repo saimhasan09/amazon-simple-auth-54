696033ebd70e86b108a622593782f8b50d6d87f1
saimhasan09 <saimhasannahid@gmail.com>
2021-10-10 17:49:48 +0600

    initialize-firebase

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4U5MjjDXrWD1fQqX0c0tuOUdyq20h-cw",
+    authDomain: "simple-f1d71.firebaseapp.com",
+    projectId: "simple-f1d71",
+    storageBucket: "simple-f1d71.appspot.com",
+    messagingSenderId: "13157843093",
+    appId: "1:13157843093:web:164801aa1e2927d010db9c"
+};
+
+export default firebaseConfig;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../../images/logo.png';
+// import logo from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
     return (
         <div className="header">
-            <img className="logo" src={logo} alt="" />
+            {/* <img className="logo" src={logo} alt="" /> */}
             <nav>
                 <NavLink to="/shop">Shop</NavLink>
                 <NavLink to="/review">Order Review</NavLink>
