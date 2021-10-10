7c2749945cf2f72dfe399e972dc6d730c15e7b45
saimhasan09 <saimhasannahid@gmail.com>
2021-10-10 17:37:21 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import OrderReview from './components/OrderReview/OrderReview';
+import PlaceOrder from './components/PlaceOrder/PlaceOrder';
+import Shop from './components/Shop/Shop';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="/placeorder">
+            <PlaceOrder></PlaceOrder>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../images/logo.png';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img className="logo" src={logo} alt="" />
+            <nav>
+                <NavLink to="/shop">Shop</NavLink>
+                <NavLink to="/review">Order Review</NavLink>
+                <NavLink to="/inventory">Manage Inventory</NavLink>
+                <NavLink to="/login">Log In</NavLink>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
